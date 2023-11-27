e56d5ef42c965899ea4a98fa9c6bc7dd0cc9fee2
MHD <mahmoud.yahafdhou83@gmail.com>
2023-11-27 17:17:58 +0100

    add tp5 ac1

---
+++ mahmoud_amar_tp5-ac1/app/src/main/java/com/example/mahmoud_amar_tp2/view/ConsulteActivity.java
@@ -0,0 +1,16 @@
+package com.example.mahmoud_amar_tp2.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mahmoud_amar_tp2.R;
+
+public class ConsulteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulte);
+    }
+}
